53b38185ad6b797909724f59ab2777d4a2b338ca
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-27 17:01:04 +0530

    added button to select location

--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:snap_savor/providers/user_places.dart';
 import 'package:snap_savor/widgets/image_input.dart';
+import 'package:snap_savor/widgets/location_input.dart';
 
 class AddPlaceScreen extends ConsumerStatefulWidget {
   const AddPlaceScreen({super.key});
@@ -61,6 +62,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
                 _selectedImage = image;
               },
             ),
+            const SizedBox(height: 10),
+            LocationInput(),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
