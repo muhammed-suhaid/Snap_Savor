25b752b3b84aad44d357637cb97327eaf6a49122
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-27 16:28:07 +0530

    updated the model, provider and read that image and saved it

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,11 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:snap_savor/models/place.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addplace(String title) {
-    final newPlace = Place(title: title);
+  void addplace(String title, File image) {
+    final newPlace = Place(title: title, image: image);
     state = [newPlace, ...state];
   }
 }
--- lib/screens/add_place.dart
+++ lib/screens/add_place.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:snap_savor/providers/user_places.dart';
@@ -12,15 +14,18 @@ class AddPlaceScreen extends ConsumerStatefulWidget {
 
 class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
   final _titleController = TextEditingController();
+  File? _selectedImage;
 
   void _savePlace() {
     final enteredTitle = _titleController.text;
 
-    if (enteredTitle.isEmpty) {
+    if (enteredTitle.isEmpty || _selectedImage == null) {
       return;
     }
 
-    ref.read(userPlacesProvider.notifier).addplace(enteredTitle);
+    ref
+        .read(userPlacesProvider.notifier)
+        .addplace(enteredTitle, _selectedImage!);
 
     Navigator.of(context).pop();
   }
@@ -42,14 +47,20 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
         child: Column(
           children: [
             TextField(
-              decoration: const InputDecoration(labelText: 'Title'),
+              decoration: const InputDecoration(
+                labelText: 'Title',
+              ),
               controller: _titleController,
               style: TextStyle(
                 color: Theme.of(context).colorScheme.onBackground,
               ),
             ),
             const SizedBox(height: 10),
-            ImageInput(),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
             const SizedBox(height: 16),
             ElevatedButton.icon(
               onPressed: _savePlace,
