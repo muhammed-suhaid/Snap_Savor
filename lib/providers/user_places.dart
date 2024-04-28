199b05c27324409abeba2692f5ce7d1abc647a5f
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-28 20:34:28 +0530

    Loaded the data as places from database

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -8,8 +8,42 @@ import 'package:sqflite/sqlite_api.dart';
 
 import 'package:snap_savor/models/place.dart';
 
+Future<Database> _getDatabase() async {
+  final dbPath = await sql.getDatabasesPath();
+  final db = await sql.openDatabase(
+    path.join(dbPath, 'places.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+    },
+    version: 1,
+  );
+  return db;
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
+
+  void loadPlaces() async {
+    final db = await _getDatabase();
+
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            id: row['id'] as String,
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              longitude: row['lng'] as double,
+              latitude: row['lat'] as double,
+              address: row['address'] as String,
+            ),
+          ),
+        )
+        .toList();
+    state = places;
+  }
 
   void addplace(String title, File image, PlaceLocation? location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
@@ -18,16 +52,7 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
     final newPlace =
         Place(title: title, image: copiedImage, location: location);
-
-    final dbPath = await sql.getDatabasesPath();
-    final db = await sql.openDatabase(
-      path.join(dbPath, 'places.db'),
-      onCreate: (db, version) {
-        return db.execute(
-            'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
-      },
-      version: 1,
-    );
+    final db = await _getDatabase();
     db.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
