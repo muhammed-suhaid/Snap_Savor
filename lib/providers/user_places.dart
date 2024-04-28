070c1b975bf6e2f85f5f94ee3dc7cb691fbbd2f1
Muhammed suhaid <muhammedsuhaid3@gmail.com>
2024-04-28 20:08:32 +0530

    Stored the image

--- lib/providers/user_places.dart
+++ lib/providers/user_places.dart
@@ -1,18 +1,23 @@
 import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 import 'package:snap_savor/models/place.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super(const []);
 
-  void addplace(String title, File image, PlaceLocation? location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addplace(String title, File image, PlaceLocation? location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final fileName = path.basename(image.path);
+    final copiedImage = await image.copy('${appDir.path}/$fileName');
+
+    final newPlace =
+        Place(title: title, image: copiedImage, location: location);
     state = [newPlace, ...state];
   }
 }
-
-
 
 final userPlacesProvider =
     StateNotifierProvider<UserPlacesNotifier, List<Place>>(
